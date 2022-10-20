478e0c0bf45c2dbcf22be3871c6e04a8e2fe3c46
Janio Jimenez <janiojimenez12@gmail.com>
2022-10-20 16:46:49 -0500

    Prueba finalizada

---
+++ README.md
@@ -0,0 +1,8 @@
+# suplosBackEnd
+Prueba suplos desarrollador backend
+
+bajar repositorio 
+
+Crear Basede datos 
+// crear el nombre de la DB => intelcost_bienes
+// correr el scrit que se encuentra en intelcost_bienes.sql => esto generará los datos 
---
+++ model/My_good.php
@@ -0,0 +1,37 @@
+<?php
+    require_once "Conection.php";
+
+    class My_good
+    {
+        private static $conexion;
+        private static $my_goods = array();
+
+        public static function getConexion(){
+            self::$conexion = Conection::conectar();
+        }
+
+        public static function getMyGood(){
+            self::getConexion();
+
+            $query = "SELECT 
+                        my.id, 
+                        g.address,
+                        c.city,
+                        g.phone,
+                        g.postal_code,
+                        t.house,
+                        g.price  
+                    FROM my_goods AS my 
+                    INNER JOIN goods AS g ON my.id_good = g.id 
+                    INNER JOIN cities AS c ON g.id_city = c.id 
+                    INNER JOIN type_of_houses AS t ON g.id_tipo = t.id";
+
+            $myGood = mysqli_query(self::$conexion, $query);
+
+            while ($fila = $myGood->fetch_assoc()) {
+                self::$my_goods[]=$fila;
+            }     
+            return self::$my_goods;
+        }        
+    }
+?>
